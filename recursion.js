b50a1bc0fdc3c2d33c7228d8cec511116c670cb7
Teddi Maull <teddimaull@gmail.com>
2016-08-09 01:27:10 -0600

    initial commit

---
+++ recursion.js
@@ -0,0 +1,21 @@
+// Define a recursive function that takes an argument n and prints the lyrics to 99 bottles of beer on the wall, starting with that number n.
+
+function bottlesRecursion(numBottles){
+  //base case
+
+  //call self with new data
+
+}
+bottlesRecursion(1);
+bottlesRecursion(5);
+
+
+// Define a recursive function that takes an argument num and computes the factorial of that number. For example num = 5 would return 120 since 5*4*3*2*1 = 120.
+function factorial(num){
+  //base case
+
+  //call self with new data
+
+}
+
+console.log(factorial(5));
