56fe8147e1c26822a5ed99554a387f538f1e4ab8
Teddi Maull <teddimaull@gmail.com>
2016-08-13 08:14:58 -0600

    add exercises from class

---
+++ filter.js
@@ -0,0 +1,46 @@
+var array = [12,2,3,90,0];
+
+//Print an array that has only numbers larger than 10
+//expect [12,90]
+
+// YOUR CODE HERE
+
+
+// console.log(gt10);
+
+//What if I want the numbers that are less than 10?
+//expect [2,3,0]
+
+// YOUR CODE HERE
+
+// console.log(lt10);
+
+////////////////////////////////////////////////////
+var students =
+[
+  {name: "Lori", grade:"3"},
+  {name: "Finn", grade:"5"},
+  {name: "Sally", grade:"3"},
+  {name: "Cory", grade:"1"}
+]
+
+
+var thirdGraders = [];
+
+// using a for loop write code that will show the info for people in 3rd grade
+// YOUR CODE HERE
+
+// console.log(thirdGraders); // Lori and Sally objects
+
+// use the filter method to produce the same results
+// YOUR CODE HERE
+
+
+// console.log(filteredThirdGraders);
+
+// What if I just want the names of the third graders? Write code that will return an array with just their names.
+
+// YOUR CODE HERE
+
+
+console.log(firstLetterOfThirdGraders);// [Lori, Sally]
